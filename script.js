1e7227e31ee6bbd123abe118cd8e378ffe3c5bda
Kapish Garg <kapishgarg2006@gmail.com>
2026-04-09 12:03:37 +0530

    Js Variables

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const title = "Macbook Pro M2 - 2022";
+const price = "$31414";
+const rating = "Rating: 5⭐️"
+
+
+
 const newCard = document.createElement("div");
 newCard.className = 'card';
 newCard.innerHTML= `
             <img
                src="https://cloud-computing-lectures-likhilesh-abcdxyz.s3.ap-south-1.amazonaws.com/likhi-laptop.jpg" />
            <div class="product-info">
-               <h4>Macbook Pro M1 - 2022</h4>
-               <p>Price: $1100</p>
-               <p>Rating: 4.9⭐️</p>
+               <h4> ${title}</h4>
+               <p> ${price} </p>
+               <p> ${rating}</p>
            </div>
 `;
 
