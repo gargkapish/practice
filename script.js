1d8bd820d025ce011da48b8decf40e8c325a8253
Vansh jain <vanshjain@Vanshs-MacBook.local>
2026-04-10 10:59:23 +0530

    vdfv

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
 
-const products = [
-    {
-        title: "Bag",
-        price: "$131",
-        rating: "4.2"
-    },
-    {
-        title: "Mat",
-        price: "$242",
-        rating: "3.5"
-    },
-    {
-        title: "Laptop",
-        price: "$3524",
-        rating: "3.6"
+
+ function main(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const newCard = document.createElement("div");
+        newCard.className = "card";
+        newCard.innerHTML = `
+        <img src="${arr[i].thumbnail}" />
+        <div class="product-info">
+            <h4> ${arr[i].title} </h4>
+            <p>Price: ${arr[i].price} </p>
+            <p>Rating: ${arr[i].rating} ⭐️</p>
+        </div>
+    `;
+        const root = document.getElementById("root");
+        root.appendChild(newCard);
     }
-]
+ }
 
-function main(arr){
-for (let i=0; i<arr.length; i++){
-const newCard = document.createElement("div");
-newCard.className = 'card';
-newCard.innerHTML= `
-            <img
-               src="https://cloud-computing-lectures-likhilesh-abcdxyz.s3.ap-south-1.amazonaws.com/likhi-laptop.jpg" />
-           <div class="product-info">
-               <h4> ${arr[i].title}</h4>
-               <p> Price: ${arr[i].price} </p>
-               <p> Raing: ${arr[i].rating}⭐️</p>
-           </div>
-`;
+//  vevresfverdf
 
-    const root = document.getElementById("root");
-    root.appendChild(newCard);
-    }
-}
-
-main(products);
-
+ async function getData() {
+    const URL = "https://dummyjson.com/products?select=title,price,rating,thumbnail";
+    const response = await fetch(URL);
+    const data = await response.json();
+    console.log(data);
+ 
+    main(data.products);
+ }
+ 
+ getData();
